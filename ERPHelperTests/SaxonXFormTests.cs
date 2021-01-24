16fd90dd9a6f6d047dae3403798598d9eb1ac0ef
Shannon Whitley <swhitley@whitleymedia.com>
2021-01-23 23:34:29 -0800

    Including Tests
    - Added the testing project to the repository. Only a few tests included so far.
    - Updated the solution to show the nested test project.

---
+++ ERPHelperTests/SaxonXFormTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ERPHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPHelperTests.Properties;
+
+namespace ERPHelper.Tests
+{
+    [TestClass()]
+    public class SaxonXFormTests
+    {
+        [TestMethod()]
+        public void TransformXmlTest()
+        {
+            string results = SaxonXForm.TransformXml(Resources.SaxonXFormXML, Resources.SaxonXFormXSLT);
+            Assert.AreEqual(Resources.SaxonXFromOutput, results);
+        }
+    }
+}
---
+++ ERPHelperTests/Workday/WDWebServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ERPHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPHelperTests.Properties;
+
+namespace ERPHelper.Tests
+{
+    [TestClass()]
+    public class WDWebServiceTests
+    {
+        [TestMethod()]
+        public void WrapSOAPTest()
+        {
+            string results = WDWebService.WrapSOAP("dummy", "dummy", Resources.WDWebServiceTest_WrapSOAPTestInput);
+            Assert.AreEqual(Resources.WDWebServiceTest_WrapSOAPTestOutput, results);
+        }
+
+        [TestMethod()]
+        public void DownloadTest()
+        {
+            string url = "https://community.workday.com/sites/default/files/file-hosting/productionapi/index.html";
+            Dictionary<string, string> results = WDWebService.Download(url);
+            Assert.AreEqual(true, results.ContainsValue("Absence_Management"));
+        }
+    }
+}
