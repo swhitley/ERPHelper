2072d341b40e98393ea8f08b261e3a1ad3c16afc
Shannon Whitley <swhitley@whitleymedia.com>
2021-02-13 00:25:32 -0800

    Release Fix
    Update to GetLatest release.

--- Octokit.cs
+++ Octokit.cs
@@ -17,12 +17,12 @@ namespace ERPHelper
             {
                 
                 GitHubClient client = new GitHubClient(new ProductHeaderValue("erp-helper"));
-                IReadOnlyList<Release> releases = await client.Repository.Release.GetAll("swhitley", "ERPHelper");
-                Version latestGitHubVersion = new Version(releases[0].TagName.Replace("v", ""));                
+                Release latestRelease = await client.Repository.Release.GetLatest("swhitley", "ERPHelper");
+                Version latestGitHubVersion = new Version(latestRelease.TagName.Replace("v", ""));                
                 string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
                 Version localVersion = new Version(version.Substring(0, version.LastIndexOf(".")));
                 release.UpdateAvailable = localVersion.CompareTo(latestGitHubVersion) != 0 ? true : false;
-                release.Description = releases[0].Body;
+                release.Description = latestRelease.Body;
                 release.Version = latestGitHubVersion.ToString();
                 
             }
