d74e16f6ce6a65a9ea2b4d38809fa017bcacff22
Shannon Whitley <swhitley@whitleymedia.com>
2021-02-20 19:03:19 -0800

    PostBuild Update
    - Update zip package from dll in plugin folder.

--- PostBuild.cmd
+++ PostBuild.cmd
@@ -1,21 +1,21 @@
 REM % 1 = Configuration Name
 REM % 2 = Platform Name
-REM % 3 = Target Path
+set file="ERPHelper.dll"
 
 if not %1 == "Release" goto exit
 
 if %2 == "x86" (
 	set dir="C:\Program Files (x86)\Notepad++\plugins\ERPHelper\"
-	set file="ERPHelper_x86.zip"
+	set zip="ERPHelper_x86.zip"
 )
 
 if %2 == "x64" (
 	set dir="C:\Program Files\Notepad++\plugins\ERPHelper\"
-	set file="ERPHelper_x64.zip"
+	set zip="ERPHelper_x64.zip"
 )
 
-if exist %dir% powershell Compress-Archive -Path '%~3' -DestinationPath '%dir:"=%%file:"=%' -Update
+if exist %dir% powershell Compress-Archive -Path '%dir:"=%%file:"=%' -DestinationPath '%dir:"=%%zip:"=%' -Update
 
-if exist %dir% powershell "Get-FileHash '%dir:"=%%file:"=%' | select -expandproperty Hash | select @{n='Hash';e={$_.toLower()}} | Out-File -FilePath '%dir:"=%sha256.txt'"
+if exist %dir% powershell "Get-FileHash '%dir:"=%%zip:"=%' | select -expandproperty Hash | select @{n='Hash';e={$_.toLower()}} | Out-File -FilePath '%dir:"=%sha256.txt'"
 
 :exit
